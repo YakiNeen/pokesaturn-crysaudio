59b36eed367d390aa551dfdee0c4ea7084edae99
YakiNeen <Felipe.12.2004@gmail.com>
2023-01-24 14:42:58 -0300

    Quest 64: Melrode Song Port

--- crysaudio/music/RBY/pallettown.asm
+++ crysaudio/music/RBY/pallettown.asm
@@ -5,316 +5,169 @@ Music_RBYPalletTown::
 	channel 3, Music_RBYPalletTown_Ch3
 
 Music_RBYPalletTown_Ch1::
-	tempo 160
+	tempo 120
 	volume 7, 7
 	duty_cycle 2
+	note_type 12, 8, 5
+
+Music_RBYPalletTown_Dois::
+	octave 3
+	note A#, 8
+	note D_, 8
+	octave 2
+	note A#, 8
+	note G#, 8
+	rest 4
+	octave 4
 	note_type 12, 12, 3
-
-Music_RBYPalletTown_branch_a7ce::
-	octave 3
-	note B_, 4
-	octave 4
-	note C_, 2
-	note D_, 4
-	note G_, 2
-	note D_, 2
-	note C_, 2
-	octave 3
-	note B_, 4
-	note G_, 2
-	octave 4
-	note D_, 4
-	note D_, 2
-	note C_, 2
-	octave 3
-	note B_, 2
-	rest 2
-	note B_, 2
-	octave 4
-	note C_, 2
-	octave 3
-	note B_, 2
-	octave 4
 	note C_, 8
-	rest 2
+	rest 4	
+	note D_, 8
+	note_type 12, 8, 5
+	rest 8
 	octave 3
-	note B_, 2
-	octave 4
-	note C_, 2
+	note A#, 8
+	note D_, 8
+	octave 2
+	note A#, 8
+	note G#, 8
+	rest 16
+	rest 16
 	octave 3
-	note A_, 2
-	note B_, 2
-	note G_, 2
-	note A_, 2
-	note F#, 2
-	note B_, 4
-	octave 4
-	note C_, 2
-	note D_, 4
-	note G_, 2
-	note D_, 2
-	note C_, 2
+	note A#, 8
+	note D_, 8
+	octave 2
+	note A#, 8
+	note G#, 8
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	octave 2
+	note A#, 8
 	octave 3
-	note B_, 4
-	note G_, 2
-	octave 4
-	note D_, 4
-	note D_, 2
-	note G_, 2
-	note F#, 2
-	note E_, 4
-	note D_, 2
+	note D#, 8
+	note D_, 8
 	note C_, 4
-	octave 3
-	note A_, 2
-	note B_, 2
-	octave 4
-	note C_, 2
-	note D_, 2
-	note C_, 2
-	octave 3
-	note B_, 2
-	note A_, 2
-	note G_, 4
-	note F#, 4
-	octave 4
-	note C_, 2
-	octave 3
-	note G_, 2
-	note E_, 2
-	note G_, 2
-	octave 4
-	note D_, 2
-	octave 3
-	note A_, 2
-	note F#, 2
-	note A_, 2
-	note_type 12, 11, 3
-	note B_, 2
-	note G_, 2
-	note D_, 2
-	note G_, 2
-	note B_, 2
-	note G_, 2
-	note D_, 2
-	note G_, 2
-	octave 4
-	note C_, 2
-	octave 3
-	note G_, 2
-	note E_, 2
-	note G_, 2
-	octave 4
-	note D_, 2
-	octave 3
-	note A_, 2
-	note F#, 2
-	note A_, 2
-	note B_, 2
-	note G_, 2
-	note D_, 2
-	note G_, 2
-	note B_, 2
-	note G_, 2
-	note D_, 2
-	note G_, 2
-	note A_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note A_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note A_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note A_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note F#, 2
-	note D_, 2
-	note C_, 2
-	note D_, 2
-	note G_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note G_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note F#, 2
-	note D_, 2
-	note C_, 2
-	note D_, 2
-	sound_loop 0, Music_RBYPalletTown_branch_a7ce
-	sound_ret
-
+	note D#, 4
+	sound_loop 0, Music_RBYPalletTown_Dois
 
 Music_RBYPalletTown_Ch2::
 	duty_cycle 2
 
-Music_RBYPalletTown_branch_a861::
-	note_type 12, 13, 3
-	octave 5
-	note D_, 2
-	note_type 12, 10, 3
-	note C_, 2
-	note_type 12, 13, 3
+Music_RBYPalletTown_Principal::
 	octave 4
-	note B_, 2
-	note_type 12, 11, 3
-	note A_, 2
-	note_type 12, 13, 3
-	octave 5
+	note_type 12, 8, -7
+	note D#, 12
+	note_type 12, 12, 5
 	note G_, 2
-	note_type 12, 11, 3
-	note E_, 2
-	note_type 12, 13, 3
-	note F#, 2
-	note E_, 2
+	note F_, 2
+	note G#, 8
+	note G_, 8
+	rest 4
+	octave 5
+	note C_, 4
 	note D_, 6
+	note D#, 2
 	octave 4
-	note B_, 2
+	note A#, 4
+	note G_, 4
+	note D_, 4
+	note D#, 2
+	note F_, 2
+	note_type 12, 8, -7
+	note D#, 12
+	note_type 12, 12, 5
 	note G_, 2
-	note G_, 2
-	note A_, 2
-	note B_, 2
+	note F_, 2
+	note G#, 8
+	note G_, 8
 	octave 5
+	note C_, 4
+	octave 4
+	note A#, 4
+	note A#, 4
+	note G#, 4
+	note G_, 8
+	note F_, 8
+	note_type 12, 8, -7
+	note D#, 12
+	note_type 12, 12, 5
+	note G_, 2
+	note F_, 2
+	note G#, 8
+	note G_, 8
+	rest 4
+	octave 5
+	note C_, 4
+	note D_, 6
+	note D#, 2
+	note G_, 4
+	note F_, 4
+	note D#, 6
+	note D_, 2
 	note C_, 10
-	octave 4
-	note F#, 2
-	note G_, 2
-	note A_, 2
-	note B_, 6
-	octave 5
-	note C_, 1
-	octave 4
-	note B_, 1
-	note A_, 8
-	octave 5
-	note D_, 2
-	note_type 12, 10, 3
-	note C_, 2
-	note_type 12, 13, 3
-	octave 4
-	note B_, 2
-	note_type 12, 11, 3
-	octave 5
-	note D_, 2
-	note_type 12, 13, 3
-	note G_, 2
-	note_type 12, 10, 3
-	note F#, 2
-	note_type 12, 11, 3
-	note F#, 2
-	note_type 12, 13, 3
-	note G_, 2
-	note E_, 6
-	note D_, 2
-	note D_, 8
-	note C_, 2
-	octave 4
-	note B_, 2
-	note A_, 2
-	note G_, 2
-	octave 5
-	note D_, 2
-	note C_, 2
-	octave 4
-	note B_, 2
-	note A_, 2
-	note G_, 10
-	note G_, 2
-	note A_, 2
-	note B_, 2
-	octave 5
-	note C_, 8
-	note D_, 6
-	note C_, 2
-	octave 4
-	note B_, 8
 	rest 2
-	note G_, 2
-	note A_, 2
-	note B_, 2
-	octave 5
-	note C_, 4
-	note C_, 4
-	note D_, 6
-	note C_, 1
-	note D_, 1
+	note D_, 2
+	note D#, 2
 	octave 4
-	note B_, 8
-	rest 2
-	note B_, 2
-	note A_, 2
-	note G_, 2
-	note A_, 8
-	note E_, 4
-	note B_, 4
-	note A_, 8
-	note G_, 4
-	note E_, 4
-	note F#, 8
-	note G_, 4
-	note B_, 4
-	note B_, 8
-	note A_, 8
-	sound_loop 0, Music_RBYPalletTown_branch_a861
-	sound_ret
-
+	note A#, 4
+	note G#, 4
+	note G_, 6
+	note F_, 2
+	note_type 12, 5, 8
+	note D#, 16
+	rest 16
+	sound_loop 0, Music_RBYPalletTown_Principal
 
 Music_RBYPalletTown_Ch3::
 	vibrato 24, 2, 8
 	note_type 12, 1, 2
 
-Music_RBYPalletTown_branch_a8e3::
-	octave 4
-	note G_, 6
-	note E_, 6
-	note F#, 4
-	note G_, 6
-	note A_, 6
-	note G_, 4
-	note E_, 6
-	note F#, 6
-	note E_, 4
-	note G_, 6
-	note E_, 6
-	note D_, 4
-	note G_, 6
-	note E_, 6
-	note F#, 4
-	note G_, 6
-	note A_, 6
-	note G_, 4
-	note E_, 6
-	note F#, 6
-	note A_, 4
-	note G_, 6
-	note E_, 6
-	note D_, 4
-	note C_, 8
-	note D_, 8
+Music_RBYPalletTown_Baixo::
+	octave 3
+	note D#, 8
+	octave 2
+	note A#, 8
+	note D#, 8
+	note A#, 8
+	note G#, 4
+	rest 16
+	rest 12
+	octave 3
+	note D#, 8
+	octave 2
+	note A#, 8
+	note D#, 8
+	note A#, 8
+	note F_, 8
 	note G_, 8
-	note E_, 4
-	note D_, 4
+	octave 3
 	note C_, 8
-	note D_, 8
-	note G_, 8
-	note A_, 4
-	note G_, 4
-	note E_, 8
-	note A_, 8
-	note E_, 8
+	octave 2
+	note A#, 8
+	octave 3
+	note D#, 8
+	octave 2
+	note A#, 8
+	note D#, 8
+	note A#, 8
+	note G#, 4
+	octave 3
+	note G#, 12
 	note G_, 8
 	note F#, 8
-	note E_, 8
-	note E_, 8
-	note F#, 8
-	sound_loop 0, Music_RBYPalletTown_branch_a8e3
-	sound_ret
+	note F_, 10
+	rest 6
+	note C_, 8
+	octave 2
+	note A#, 8
+	octave 3
+	note D#, 4
+	rest 12
+	rest 8
+	octave 2
+	note A#, 4
+	octave 3
+	note D#, 4
+	sound_loop 0, Music_RBYPalletTown_Baixo
