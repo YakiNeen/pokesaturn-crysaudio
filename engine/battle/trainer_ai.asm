8422403a078316bc96bc3c43e53a2e303aab52cb
YakiNeen <Felipe.12.2004@gmail.com>
2023-01-24 19:49:27 -0300

    Fix Cooltrainer Girl and Blaine AI

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -342,10 +342,8 @@ CooltrainerMAI:
 	jp AIUseXAttack
 
 CooltrainerFAI:
-	; The intended 25% chance to consider switching will not apply.
-	; Uncomment the line below to fix this.
 	cp 25 percent + 1
-	; ret nc
+	ret nc
 	ld a, 10
 	call AICheckIfHPBelowFraction
 	jp c, AIUseHyperPotion
@@ -387,7 +385,10 @@ KogaAI:
 BlaineAI:
 	cp 25 percent + 1
 	ret nc
-	jp AIUseSuperPotion
+	ld a, 10
+	call AICheckIfHPBelowFraction
+	ret nc
+	jp AIUseHyperPotion
 
 SabrinaAI:
 	cp 25 percent + 1
