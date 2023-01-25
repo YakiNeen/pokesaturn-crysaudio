db43cba4908e8b6aba5c4e147b040ac663119607
YakiNeen <Felipe.12.2004@gmail.com>
2023-01-25 10:17:11 -0300

    OAM updates can be interrupted by V-Blank

--- home/update_sprites.asm
+++ home/update_sprites.asm
@@ -2,5 +2,9 @@ UpdateSprites::
 	ld a, [wUpdateSpritesEnabled]
 	dec a
 	ret nz
+	ld hl, hSkipOAMUpdates
+	res 0, [hl]
 	homecall _UpdateSprites
+	ld hl, hSkipOAMUpdates
+	res 0, [hl]
 	ret
--- home/vblank.asm
+++ home/vblank.asm
@@ -26,6 +26,9 @@ VBlank::
 	call VBlankCopy
 	call VBlankCopyDouble
 	call UpdateMovingBgTiles
+	ld a, [hSkipOAMUpdates]
+	bit 0, a
+	jr nz, .skipOAM
 	call hDMARoutine
 	ld a, BANK(PrepareOAMData)
 	ldh [hLoadedROMBank], a
@@ -34,6 +37,7 @@ VBlank::
 
 	; VBlank-sensitive operations end.
 
+.skipOAM
 	call Random
 
 	ldh a, [hVBlankOccurred]
