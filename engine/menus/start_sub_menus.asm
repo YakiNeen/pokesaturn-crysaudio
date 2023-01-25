99e553cdf887d1d0beda53b000b3df525e9eab95
YakiNeen <Felipe.12.2004@gmail.com>
2023-01-25 10:18:23 -0300

    Trainer Card transition screens can show brief garbage on DMG

--- engine/menus/start_sub_menus.asm
+++ engine/menus/start_sub_menus.asm
@@ -467,6 +467,9 @@ StartMenu_TrainerInfo::
 	predef DrawBadges ; draw badges
 	ld b, SET_PAL_TRAINER_CARD
 	call RunPaletteCommand
+	ld a, [wOnSGB]
+	and a
+	call z, Delay3
 	call GBPalNormal
 	call WaitForTextScrollButtonPress ; wait for button press
 	call GBPalWhiteOut
@@ -474,6 +477,9 @@ StartMenu_TrainerInfo::
 	call LoadScreenTilesFromBuffer2 ; restore saved screen
 	call RunDefaultPaletteCommand
 	call ReloadMapData
+	ld a, [wOnSGB]
+	and a
+	call z, Delay3
 	call LoadGBPal
 	pop af
 	ldh [hTileAnimations], a
