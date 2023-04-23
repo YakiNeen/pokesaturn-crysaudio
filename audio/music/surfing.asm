cdd1cdf904be7d020dd101e7df403e0020c32a0d
YakiNeen <Felipe.12.2004@gmail.com>
2023-04-23 00:45:05 -0300

    YYH Opening Theme (To Finish)

--- audio/music/surfing.asm
+++ audio/music/surfing.asm
@@ -24,19 +24,238 @@ Music_Surfing_Ch2::
 	duty_cycle 3
 	vibrato 16, 2, 5
 	note_type 12, 12, 6
+	;
+	rest 4
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
 
 Music_Surfing_branch_7faae::
 	note_type 12, 12, 6
 	;
+	octave 4
+	note D#, 3
+	note D#, 3
+	note D#, 4
+	octave 3
+	note A#, 2
+	note A_, 2
+	note A#, 2
+	;
+	octave 4
+	note D#, 3
+	note D#, 3
+	note D#, 4
+	octave 3
+	note A#, 2
+	note A_, 2
+	note A#, 2
+	;
+	octave 4
+	note D#, 3
+	note D#, 3
+	note D#, 4
+	note F_, 2
+	note G_, 2
+	note C_, 16
+	;
+	rest 2
+	;
+	octave 4
+	note D#, 3
+	note D#, 3
+	note D#, 4
+	octave 3
+	note A#, 2
+	note A_, 2
+	note A#, 2
+	;
+	octave 4
+	note D#, 3
+	note D#, 3
+	note D#, 4
+	octave 3
+	note A#, 2
+	note A_, 2
+	note A#, 2
+	;
+	octave 4
+	note D#, 3
+	note D#, 3
+	note D#, 4
+	note F_, 2
+	note G_, 2
+	note C_, 10
+	;
+	note D#, 2
+	note F_, 2
+	note G_, 2
+	note G#, 2
+	;
+	note A#, 16
+	;
+	rest 6
+	note A#, 4
+	note G#, 2
+	note G_, 2
+	note G#, 2
+	;
+	note G_, 2
+	octave 3
+	note A#, 8
+	note A#, 2
+	octave 4
+	note F_, 2
+	note D#, 12
+	;
+	note D#, 2
+	note D_, 2
+	note D#, 2
+	;
+	note C_, 3
+	note D_, 3
+	note D#, 8
+	rest 2
+	;
+	note D#, 3
+	note F_, 3
+	note G_, 8
+	rest 2
+	;
+	note G_, 3
+	note F#, 3
+	note F_, 2
+	note G_, 2
+	note G_, 2
+	note F#, 2
+	pitch_slide 1, 3, F_
+	note F_, 2
+	;
 	rest 16
+	;
+	rest 2
+	note G_, 2
+	note F_, 1
+	note G_, 2
+	note G_, 7
+	note G_, 2
+	;
+	note G_, 4
+	note F_, 2
+	note A#, 4
+	note G#, 2
+	note G_, 2
+	note F_, 2
+	;
+	note F_, 4
+	note G_, 2
+	octave 3
+	note A#, 10
+	;
+	octave 4
+	note F_, 4
+	note G_, 2
+	note C_, 10
+	;
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note C_, 2
+	note D#, 6
+	note G_, 4
+	;
+	note D#, 2
+	note F_, 4
+	note D#, 2
+	note D#, 6
+	;
+	note D_, 4
+	note D#, 2
+	note D_, 4
+	note D#, 2
+	note F_, 2
+	note D#, 2
+	;
+	note G_, 16
+	;
+	rest 2
+	note G_, 2
+	note F_, 1
+	note G_, 2
+	note G_, 7
+	note G_, 1
+	note G_, 1
+	;
+	note G_, 4
+	note F_, 2
+	note A#, 4
+	note G#, 2
+	note G_, 2
+	note F_, 2
+	;
+	note F_, 4
+	note G_, 2
+	octave 3
+	note A#, 10
+	;
+	octave 4
+	note F_, 4
+	note G_, 2
+	note C_, 10
+	;
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note C_, 2
+	note D#, 6
+	note G_, 4
+	;
+	note D#, 2
+	note F_, 2
+	note C_, 2
+	note D#, 2
+	note D#, 6
+	;
+	note D_, 4
+	note D#, 2
+	note D_, 4
+	note D#, 2
+	note F_, 2
+	note D#, 2
+	;
+	pitch_slide 8, 5, A#
+	note A#, 16
+	;
+	note C_, 3
+	note D_, 3
+	note D#, 2
+	note D#, 3
+	note F_, 3
+	note G_, 2
+	;	
+	note G#, 11
+	note G_, 4
+	rest 1
 	sound_loop 0, Music_Surfing_branch_7faae
 
 
 Music_Surfing_Ch3::
 	note_type 12, 1, 0
+	;
 	rest 4
-
-Music_Surfing_branch_7fb03::
 	;
 	octave 3
 	note F#, 2
@@ -102,7 +321,163 @@ Music_Surfing_branch_7fb03::
 	rest 2
 	octave 4
 	note C#, 4
+
+Music_Surfing_branch_7fb03::
 	;
+	octave 3
+	note F#, 2
+	rest 1
+	note G#, 2
+	rest 1
+	note A#, 4
+	rest 2
+	note A#, 4
+	;
+	note F_, 2
+	rest 1
+	note F#, 2
+	rest 1
+	note G#, 4
+	rest 2
+	note G#, 4
+	;
+	note D#, 2
+	rest 1
+	note F_, 2
+	rest 1
+	note F#, 4
+	rest 2
+	note F#, 4
+	;
+	note D#, 2
+	rest 1
+	note F_, 2
+	rest 1
+	note F#, 4
+	rest 2
+	note F#, 4
+	;
+	octave 3
+	note F#, 2
+	rest 1
+	note G#, 2
+	rest 1
+	note A#, 4
+	rest 2
+	note A#, 4
+	;
+	note F_, 2
+	rest 1
+	note F#, 2
+	rest 1
+	note G#, 4
+	rest 2
+	note G#, 4
+	;
+	note D#, 2
+	rest 1
+	note F_, 2
+	rest 1
+	note F#, 4
+	rest 2
+	note F#, 4
+	;
+	note D#, 2
+	rest 1
+	note F_, 2
+	rest 1
+	note F#, 4
+	rest 2
+	note F#, 4
+	;
+	note F#, 2
+	rest 1
+	note A_, 2
+	rest 1
+	note A#, 4
+	rest 2
+	note A#, 2
+	note A_, 2
+	;
+	note G#, 2
+	rest 1
+	note A#, 2
+	rest 1
+	octave 4
+	note C_, 4
+	rest 2
+	note C_, 4
+	;
+	octave 3
+	note F_, 2
+	rest 1
+	note F#, 2
+	rest 1
+	note G#, 4
+	rest 2
+	note G#, 4
+	;
+	note G_, 2
+	rest 1
+	note G#, 2
+	rest 1
+	note A#, 4
+	rest 2
+	note A#, 2
+	rest 2
+	;
+	note D#, 3
+	note F_, 3
+	note F#, 10
+	;
+	octave 4
+	note C#, 3
+	octave 3
+	note B_, 3
+	note A#, 10
+	;
+	note F#, 2
+	rest 10
+	pitch_slide 1, 2, F_
+	note F#, 4
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
+	;
+	rest 16
 	sound_loop 0, Music_Surfing_branch_7fb03
 
 Music_Surfing_Ch4::
