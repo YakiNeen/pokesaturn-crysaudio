017f5b5b2b5984e7ee8b205996c8270b239a5100
YakiNeen <Felipe.12.2004@gmail.com>
2023-06-12 21:34:49 -0300

    Yoshi no Tamago: Music B - Fire Flower to Dungeon0

--- audio/music/dungeon0.asm
+++ audio/music/dungeon0.asm
@@ -1,42 +1,835 @@
+; Yoshi (US)/Mario & Yoshi (EU)/Yoshi no Tamago (JP) - Music B: Fire Flower
 Music_Dungeon0::
-	channel_count 4
+	channel_count 3
 	channel 1, Music_Dungeon0_Ch1
 	channel 2, Music_Dungeon0_Ch2
 	channel 3, Music_Dungeon0_Ch3
-	channel 4, Music_Dungeon0_Ch4
 
 Music_Dungeon0_Ch1::
-	tempo 120
+	tempo 224
 	volume 7, 7
 	duty_cycle 2
-	note_type 12, 12, 8
+	toggle_perfect_pitch
+	note_type 12, 11, 3
+	rest 4
+	octave 4
+	note C_, 8
+	octave 3
+	note B_, 8
+	note E_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 10
+	rest 16
+	rest 10
+	octave 3
+	note G_, 1
+	note F_, 1
+	note E_, 1
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note C_, 2
+	note E_, 2
+	note D_, 2
+	note F_, 2
+	note C_, 2
+	note E_, 2
+	note D_, 2
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 2
+	note F_, 2
+	note G_, 4
+	note F_, 4
 
 Music_Dungeon0_Dois::
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note G_, 2
+	note F_, 2
+	note C_, 2
+	note E_, 2
+	note C_, 2
+	note D_, 2
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note F_, 1
+	note E_, 1
+	note D_, 2
+	note G_, 2
+	note C_, 2
+	note D_, 2
+	note E_, 2
+	note D_, 2
+	note E_, 1
+	note D_, 1
+	note C_, 2
+	note E_, 2
+	note G_, 2
+	note D_, 4
+	note F_, 4
+	note G_, 2
+	note F_, 2
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note G_, 2
+	note C_, 2
+	note E_, 1
+	note G_, 1
+	note F_, 4
+	note G_, 2
+	note E_, 2
+	note F_, 2
+	note F_, 2
+	note E_, 2
+	note E_, 2
+	note D_, 2
+	note F_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note F_, 2
+	note G_, 1
+	note F_, 1
+	note E_, 2
+	note E_, 2
+	note G_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 2
+	note C_, 2
+	note D_, 1
+	note C_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 2
+	note E_, 2
+	note F_, 2
+	note E_, 2
+	note F_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 2
+	octave 2
+	note B_, 2
+	note A_, 2
+	note B_, 2
+	octave 3
+	note C_, 2
+	note C_, 2
+	note D_, 1
+	note C_, 1
+	octave 2
+	note B_, 1
+	note A_, 1
+	octave 3
+	note C_, 2
+	note E_, 2
+ 	note F_, 1
+	note D_, 1
+	note E_, 1
+	note C_, 1
+	note D_, 16
+	rest 4
+	rest 2
+	note G_, 4
+	note G_, 8
+	note F_, 2
+	note E_, 16
+	rest 12
+	note G_, 2
+	note F_, 2
+	note E_, 2
+	note F_, 2
+	note G_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 2
+	note E_, 2
+	note G_, 2
+	note A_, 1
+	note F_, 1
+	note G_, 1
+	note E_, 1
+	note F_, 2
+	note E_, 1
+	note G_, 1
+	note A_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	octave 4
+	note D_, 2
+	note_type 12, 10, 5
+	note E_, 8
+	note C_, 8
+	note F_, 8
+	note D_, 4
+	note E_, 4
+	note G_, 8
+	note E_, 4
+	note B_, 4
+	note E_, 8
+	note E_, 8
+	note C_, 8
+	octave 3
+	note B_, 8
+	note A_, 8
+	note G_, 8
+	note A_, 8
+	note B_, 8
+	note_type 12, 11, 3
+	note G_, 4
+	note F_, 4
+	note E_, 4
+	note D_, 4
+	octave 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	note A_, 4
+	note G_, 2
+	note B_, 2
+	octave 4
+	note C_, 4
+	octave 3
+	note F_, 4
+	note G_, 4
+	note C_, 4
+	note D_, 4
+	note E_, 4
+	note F_, 4
+	note G_, 16
+	rest 4
+	rest 16
+	rest 16
 	rest 16
 	sound_loop 0, Music_Dungeon0_Dois
 
 
 Music_Dungeon0_Ch2::
 	duty_cycle 2
-	note_type 12, 12, 8
+	note_type 12, 12, 3
+	octave 4
+	note C_, 2
+	note D_, 2
+	note E_, 4
+	note F_, 2
+	note E_, 2
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note C_, 4
+	note D_, 4
+	note C_, 2
+	octave 3
+	note G_, 2
+	note F_, 2
+	note G_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note C_, 2
+	note E_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note C_, 1
+	note D_, 1
+	note E_, 2
+	note F_, 2
+	note E_, 4
+	note D_, 2
+	note C_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note G_, 2
+	note F_, 2
+	note C_, 2
+	note E_, 2
+	note C_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note F_, 1
+	note E_, 1
+	note D_, 4
 
 Music_Dungeon0_Principal::
-	rest 16
+	rest 2
+	note C_, 1
+	note D_, 1
+	note E_, 2
+	note F_, 1
+	note E_, 1
+	note D_, 2
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 1
+	note D_, 2
+	note E_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note B_, 2
+	octave 4
+	note G_, 1
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	octave 4
+	note E_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note E_, 2
+	note F_, 2
+	note D_, 1
+	note E_, 1
+	note F_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note F_, 2
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note C_, 1
+	note D_, 1
+	note E_, 2
+	note D_, 2
+	note C_, 2
+	note D_, 2
+	note E_, 2
+	note C_, 2
+	note D_, 1
+	note C_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 1
+	note D_, 2
+	note E_, 2
+	note F_, 8
+	note E_, 2
+	note E_, 2
+	note E_, 1
+	note D_, 1
+	note C_, 1
+	note D_, 1
+	note C_, 2
+	note C_, 1
+	octave 3
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note A_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note C_, 2
+	note D_, 4
+	note E_, 2
+	note E_, 2
+	note E_, 1
+	note D_, 1
+	note C_, 1
+	note D_, 1
+	note C_, 2
+	note C_, 1
+	octave 3
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note A_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note B_, 2	
+	octave 4
+	note C_, 2
+	note D_, 4
+	note C_, 1
+	octave 3
+	note B_, 1
+	note A_, 2
+	note G_, 2
+	octave 4
+	note E_, 2
+	octave 3
+	note G_, 2
+ 	octave 4
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 2
+	note C_, 1
+	note D_, 1
+	octave 3
+	note B_, 2
+	octave 4
+	note C_, 2
+	note D_, 4
+	note G_, 1
+	note E_, 1
+	note F_, 1
+	note D_, 1
+	note E_, 1
+	note C_, 1
+	note D_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 4
+	note G_, 1
+	note E_, 1
+	note F_, 1
+	note D_, 1
+	note E_, 1
+	note C_, 1
+	note D_, 1
+	octave 3
+	note B_, 1
+ 	octave 4
+	note C_, 2
+	note C_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	note F_, 4
+	note G_, 1
+	note E_, 1
+	note F_, 1
+	note D_, 1
+	note E_, 1
+	note C_, 1
+	note D_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 2
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 1
+	note D_, 1
+	note F_, 2
+	note F_, 2
+	note E_, 2
+	note E_, 2
+	note D_, 2
+	note E_, 2
+	note F_, 4
+	note_type 12, 11, 7
+	note G_, 8
+	note E_, 8
+	note A_, 8
+	note F_, 8
+	note B_, 8
+	note G_, 8
+	note_type 12, 12, 3
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note B_, 2
+	note B_, 2
+	note A_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 1
+	note D_, 1
+	note C_, 1
+	octave 4
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note E_, 1
+	note F_, 1
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 1
+	note E_, 1
+	note F_, 2
+	note E_, 1
+	note F_, 1
+	note G_, 2
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	note G_, 2
+	note G_, 1
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	octave 4
+	note D_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note E_, 8	
+	note G_, 8
+	note E_, 2
+	note E_, 2
+	note E_, 1
+	note D_, 1
+	note C_, 1
+	note D_, 1
+	note C_, 2
+	note C_, 1
+	octave 3
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note A_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+ 	octave 4
+	note C_, 2
+	note D_, 4
+	note C_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note C_, 2
+	note E_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note C_, 1
+	note D_, 1
+	note E_, 2
+	note F_, 2
+ 	note E_, 2
+	note C_, 2
+	note D_, 4
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note G_, 2
+	note F_, 2
+	note C_, 2
+	note E_, 2
+	note C_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note F_, 2
+	note E_, 4
+	note D_, 4
 	sound_loop 0, Music_Dungeon0_Principal
 
 
 Music_Dungeon0_Ch3::
-	note_type 12, 1, 2
+	note_type 12, 1, 0
+	vibrato 8, 2, 6
+	rest 4
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 12
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 1
 
 Music_Dungeon0_Baixo::
+	octave 3
+	note C_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note C_, 2
+	note E_, 2
+	note D_, 2
+	note F_, 2
+	note C_, 2
+	note E_, 2
+	note D_, 1
+	note C_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note B_, 2
+	octave 3
+	note C_, 2
+	note D_, 4
+	note G_, 4
 	rest 16
+	rest 16
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note G_, 2
+	note F_, 4
+	note E_, 4
+	note D_, 3
+	rest 1
+	note D_, 1
+ 	note C_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 4
+	note G_, 4
+ 	rest 16
+	rest 16
+	rest 16
+	note F_, 2
+	note E_, 2
+	note D_, 1
+	note C_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 4
+	note F_, 1	
+	note G_, 1
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C_, 1
+	rest 1	
+	note C_, 1
+	rest 1
+	note C_, 1
+	octave 3
+	note B_, 1
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C_, 2
+	note D_, 2
+	note E_, 2
+	note C_, 2
+	note D_, 4
+	note C_, 4
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note B_, 2
+	octave 4
+	note C_, 2
+	rest 6
+	note G_, 1
+	note E_, 1
+	note F_, 1
+	note D_, 1
+	note E_, 1
+	note C_, 1
+	note D_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 4
+	rest 12
+	rest 12
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note F_, 2
+	note G_, 2
+	note A_, 4
+ 	note B_, 2
+	note G_, 2
+	octave 4
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note F_, 1
+	note G_, 8
+	vibrato 8, 4, 6
+	note G_, 4
+	vibrato 8, 2, 6
+	note D_, 1
+	note E_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 8
+	vibrato 8, 4, 6
+	note A_, 4
+	vibrato 8, 2, 6
+	note E_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 1
+	note B_, 4
+	note A_, 2
+	note B_, 2
+	note G_, 2
+	note B_, 2
+	octave 5
+	note C_, 8
+	octave 4
+	note B_, 8
+	note A_, 8
+	note G_, 8
+	note F_, 8
+	note E_, 8
+	note D_, 8
+	note G_, 8
+	note C_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	octave 3
+	note B_, 1
+	rest 1
+	note B_, 1
+	rest 1
+	note A_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	note G_, 2
+	octave 4
+	note C_, 1
+	rest 1
+	note C_, 1
+	note D_, 1
+	note C_, 1
+	octave 3
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note E_, 1
+	note F_, 1
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 1
+	note E_, 1
+	note F_, 2
+	note E_, 1
+	note F_, 1
+	note G_, 2
+	note F_, 1
+	note G_, 1
+	note A_, 2
+	note B_, 2
+	octave 4
+	note D_, 2
+	note C_, 8
+	note D_, 8
+	note E_, 8
+	note F_, 8
+	note G_, 8
+	note F_, 4
+	note E_, 4
+	note D_, 8
+	note E_, 4
+	note F_, 4
 	sound_loop 0, Music_Dungeon0_Baixo
-
-
-Music_Dungeon0_Ch4::
-	toggle_noise 0
-	drum_speed 12
-
-Music_Dungeon0_Percussao::
-	rest 16
-	sound_loop 0, Music_Dungeon0_Percussao
