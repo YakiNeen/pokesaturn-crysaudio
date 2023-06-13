af80b53304378bc4cb11fad88648442cdf12b584
YakiNeen <Felipe.12.2004@gmail.com>
2023-06-13 11:35:36 -0300

    Vermilion City Theme

--- audio/music/town5.asm
+++ audio/music/town5.asm
@@ -1,3 +1,4 @@
+; Junichi Masuda - Vermilion City
 Music_Cidades5::
 	channel_count 4
 	channel 1, Music_Cidades5_Ch1
@@ -6,30 +7,286 @@ Music_Cidades5::
 	channel 4, Music_Cidades5_Ch4
 
 Music_Cidades5_Ch1::
-	tempo 120
+	tempo 156
 	volume 7, 7
-	duty_cycle 2
-	note_type 12, 12, 8
+	duty_cycle 3
+	vibrato 12, 3, 4
+	toggle_perfect_pitch
 
 Music_Cidades5_Dois::
-	rest 16
+	note_type 12, 11, 5
+	octave 3
+	note E_, 4
+	note C#, 1
+	note D_, 1
+	note E_, 2
+	note A_, 4
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note E_, 2
+	note F#, 2
+	note A_, 4
+	note F#, 1
+	note G#, 1
+	note A_, 2
+	note E_, 4
+	note C#, 2
+	note E_, 2
+	note A_, 2
+	note G#, 2
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	note C#, 2
+	note D_, 2
+	note E_, 2
+	note F#, 2
+	note E_, 4
+	note C#, 1
+	note D_, 1
+	note E_, 2
+	note A_, 4
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note E_, 2
+	note F#, 2
+	note A_, 4
+	note F#, 1
+	note G#, 1
+	note A_, 2
+	note E_, 4
+	note C#, 1
+	note D_, 1
+	note E_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note A_, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	note F#, 4
+	note E_, 4
+	note F#, 2
+	note G#, 2
+	note F#, 2
+	note A_, 2
+	note G#, 2
+	note B_, 2
+	note A_, 2
+	octave 4
+	note C#, 2
+	note D_, 2
+	note C#, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G#, 1
+	note A_, 1
+	note B_, 2
+	octave 4
+	note C#, 2
+	note E_, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note D_, 2
+	octave 3
+	note G#, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note F#, 2
+	note B_, 2
+	note G#, 2
+	note A_, 2
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	note B_, 2
 	sound_loop 0, Music_Cidades5_Dois
 
 
 Music_Cidades5_Ch2::
-	duty_cycle 2
-	note_type 12, 12, 8
+	duty_cycle 3
+	vibrato 10, 2, 3
 
 Music_Cidades5_Principal::
-	rest 16
+	note_type 12, 12, 7
+	octave 3
+	note A_, 8
+	octave 4
+	note D_, 4
+	note C#, 4
+	octave 3
+	note B_, 6
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C#, 8
+	octave 3
+	note A_, 8
+	octave 4
+	note D_, 4
+	note C#, 4
+	octave 3
+	note B_, 6
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A_, 8
+	note A_, 8
+	octave 4
+	note D_, 4
+	note C#, 4
+	octave 3
+	note B_, 6
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C#, 8
+	octave 3
+	note A_, 8
+	octave 4
+	note D_, 4
+	note C#, 4
+	octave 3
+	note B_, 6
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A_, 8
+	note B_, 4
+	octave 4
+	note C#, 4
+	note D_, 4
+	note E_, 4
+	note F#, 8
+	note B_, 8
+	note A_, 4
+	note G#, 4
+	note F#, 4
+	note E_, 4
+	note F#, 8
+	note E_, 8
 	sound_loop 0, Music_Cidades5_Principal
 
 
 Music_Cidades5_Ch3::
-	note_type 12, 1, 2
+	note_type 12, 1, 0
 
 Music_Cidades5_Baixo::
-	rest 16
+	octave 4
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note G#, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note B_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note F#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note B_, 2
+	note E_, 2
+	note B_, 2
+	note E_, 2
+	note B_, 2
+	note E_, 2
+	note A_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
 	sound_loop 0, Music_Cidades5_Baixo
 
 
@@ -38,5 +295,38 @@ Music_Cidades5_Ch4::
 	drum_speed 12
 
 Music_Cidades5_Percussao::
-	rest 16
+	sound_call Music_Vermilion_branch_bb3f
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 1
+	sound_loop 4, Music_Cidades5_Percussao
+	sound_call Music_Vermilion_branch_bb3f
+	sound_call Music_Vermilion_branch_bb3f
+	sound_call Music_Vermilion_branch_bb3f
+	sound_call Music_Vermilion_branch_bb3f
 	sound_loop 0, Music_Cidades5_Percussao
+
+Music_Vermilion_branch_bb3f::
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 6, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	sound_ret
