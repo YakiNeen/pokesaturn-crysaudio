fb7bbaf4e7b2842adf039ee0993d56ec2b905f2f
YakiNeen <Felipe.12.2004@gmail.com>
2023-06-13 11:35:19 -0300

    Lavender Town Theme

--- audio/music/town4.asm
+++ audio/music/town4.asm
@@ -1,3 +1,4 @@
+; Junichi Masuda - Lavender Town
 Music_Cidades4::
 	channel_count 4
 	channel 1, Music_Cidades4_Ch1
@@ -6,37 +7,203 @@ Music_Cidades4::
 	channel 4, Music_Cidades4_Ch4
 
 Music_Cidades4_Ch1::
-	tempo 120
+	tempo 152
 	volume 7, 7
-	duty_cycle 2
-	note_type 12, 12, 8
+	duty_cycle 1
+	toggle_perfect_pitch
+	vibrato 0, 8, 8
+	note_type 12, 8, 7
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	note_type 12, 10, 7
 
 Music_Cidades4_Dois::
+	octave 3
+	note G_, 8
+	note G_, 8
+	note E_, 8
+	note E_, 8
+	note G_, 4
+	note F#, 4
+	note E_, 4
+	note B_, 4
+	note C#, 8
+	note C#, 8
+	note G_, 8
+	note G_, 8
+	note F#, 8
+	note F#, 8
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	octave 4
+	note C_, 8
+	note C_, 8
+	octave 3
+	note G_, 8
+	note G_, 8
+	note E_, 8
+	note E_, 8
+	note G_, 4
+	note F#, 4
+	note E_, 4
+	note B_, 4
+	note C#, 8
+	note C#, 8
+	note G_, 8
+	note G_, 8
+	note F#, 8
+	note F#, 8
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	note C_, 8
+	note C_, 8
+	rest 16
+	rest 16
+	rest 16
 	rest 16
 	sound_loop 0, Music_Cidades4_Dois
 
 
 Music_Cidades4_Ch2::
-	duty_cycle 2
-	note_type 12, 12, 8
+	vibrato 0, 3, 4
+	duty_cycle 3
+	note_type 12, 9, 1
 
 Music_Cidades4_Principal::
-	rest 16
+	octave 5
+	note C_, 4
+	note G_, 4
+	note B_, 4
+	note F#, 4
 	sound_loop 0, Music_Cidades4_Principal
 
 
 Music_Cidades4_Ch3::
-	note_type 12, 1, 2
+	vibrato 4, 1, 1
+	note_type 12, 3, 15
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	note_type 12, 2, 15
+	load_wave 2, 1, 14, 2, 3, 3, 2, 8, 14, 1, 2, 2, 15, 15, 14, 10, 1, 0, 1, 4, 13, 12, 1, 0, 14, 3, 4, 1, 5, 1, 7, 3
 
 Music_Cidades4_Baixo::
-	rest 16
+	octave 4
+	note E_, 16
+	note D_, 16
+	note C_, 16
+	note E_, 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note E_, 4
+	note E_, 16
+	note D_, 16
+	note C_, 16
+	note E_, 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note E_, 4
+	note E_, 16
+	note D_, 16
+	note C_, 16
+	note E_, 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note E_, 4
+	note_type 12, 3, 15
+	octave 6
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	note_type 12, 2, 15
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	octave 7
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	octave 4
+	note E_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	note E_, 16
+	note D_, 16
+	note C_, 16
+	note E_, 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note E_, 4
+	note E_, 16
+	note D_, 16
+	note C_, 16
+	note E_, 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note E_, 4
+	note E_, 16
+	note D_, 16
+	note C_, 16
+	note E_, 4
+	note C_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note E_, 4
+	note_type 12, 2, 15
+	octave 6
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	octave 7
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	octave 8
+	note B_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
+	octave 4
+	note E_, 4
+	note G_, 4
+	note F#, 4
+	note B_, 4
 	sound_loop 0, Music_Cidades4_Baixo
 
 
 Music_Cidades4_Ch4::
 	toggle_noise 0
 	drum_speed 12
+	rest 16
+	rest 16
+	rest 16
+	rest 16
 
 Music_Cidades4_Percussao::
-	rest 16
+	drum_note 7, 8
+	drum_note 7, 8
 	sound_loop 0, Music_Cidades4_Percussao
