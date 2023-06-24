9277784bb5cc266a065e624c8f602a6b921e37e2
YakiNeen <Felipe.12.2004@gmail.com>
2023-06-23 22:57:06 -0300

    No Wild Battle and Trainer Engaging When not have Mon

--- home/trainers.asm
+++ home/trainers.asm
@@ -127,6 +127,9 @@ IF DEF(_DEBUG)
 	call DebugPressedOrHeldB
 	jr nz, .trainerNotEngaging
 ENDC
+	ld a, [wPartyCount]
+	and a
+	jp z, .trainerNotEngaging
 	call CheckForEngagingTrainers
 	ld a, [wSpriteIndex]
 	cp $ff
